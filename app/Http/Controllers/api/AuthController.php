7538971889ace5307dfb2804e1841ac9b7288370
JanreyDev <janreyminadev@gmail.com>
2025-12-17 14:42:06 +0800

    Used JWT Token Instead of Sanctuam

--- app/Http/Controllers/api/AuthController.php
+++ app/Http/Controllers/api/AuthController.php
@@ -3,29 +3,26 @@
 namespace App\Http\Controllers\api;
 
 use App\Http\Controllers\Controller;
-use App\Models\User;
-use Illuminate\Support\Facades\Auth;
 use Illuminate\Http\Request;
-
+use Tymon\JWTAuth\Facades\JWTAuth;
 
 class AuthController extends Controller
 {
-    public function login(Request $request)
-    {
-        $request->validate([
-            'email' => 'required|email',
-            'password' => 'required'
-        ]);
+    
 
-        if (!Auth::attempt($request->only('email', 'password'))) {
-            return response()->json(['message' => 'Invalid login details'], 401);
-        }
-        $user = User::where('email', $request->email)->first();
-        $token = $user->createToken('auth_token')->plainTextToken;
+public function login(Request $request)
+{
+    $credentials = $request->only('email', 'password');
 
-        $user = User::where('email', $request->email)->first();
-        $token = $user->createToken('auth_token')->plainTextToken;
-
-        return response()->json(['message' => 'Login successful', 'access_token' => $token, 'token_type' => 'Bearer', 'user' => $user,]);
+    if (!$token = JWTAuth::attempt($credentials)) {
+        return response()->json(['message' => 'Invalid login details'], 401);
     }
+
+    return response()->json([
+        'message'      => 'Login successful',
+        'access_token' => $token,
+        'token_type'   => 'Bearer',
+        'user'         => auth()->user(),
+    ]);
+}
 }
